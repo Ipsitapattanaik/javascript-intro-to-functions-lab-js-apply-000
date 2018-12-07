c67e29db5f65421327674e50d973046cef95b802
ipsita Pattanaik <ipsitapattanaik.cse@gmail.com>
2018-12-07 03:38:45 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,21 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+function sayHiToGrandma(string) {
+  return string.toUpperCase()
+}
+function sayHiToGrandma(string) {
+    if (string.toLowerCase() === 'hello') {
+        return "I can't hear you!";
+    }
+    else if (string.toUpperCase() === 'HELLO') {
+        return "YES INDEED!";
+    }
+     else if (string.toUpperCase() === 'HELLO') {
+        return "YES INDEED!";
+    }
+    
+}
